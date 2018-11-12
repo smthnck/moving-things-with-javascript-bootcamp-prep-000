534c4c4deed007c1980895460fae4281109eb94d
Nick Smith <smthnck@gmail.com>
2018-11-12 19:13:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ document.addEventListener('keydown',function(e){if(e.which===37){moveDodgerLeft(
 function moveDodgerRight(){
   var rightNumbers = dodger.style.left.replace('px','');
   var right = parseInt(rightNumbers,10);
-  if(right>360){
+  if(right<360){
     dodger.style.left = `${right+1}px`}
 }
 
