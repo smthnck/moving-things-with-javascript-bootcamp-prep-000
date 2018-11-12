6d4eedb966156b797e13757682f34deec2b355a9
Nick Smith <smthnck@gmail.com>
2018-11-12 18:55:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,5 @@ function moveDodgerLeft(){var leftNumbers=dodger.style.left.replace('px','');var
 document.addEventListener('keydown',function(e){if(e.which===37){moveDodgerLeft();}})
 
 function moveDodgerRight(){var rightNumbers = dodger.style.right.replace('px','');var right = parseInt(rightNumbers,10);if(right>0){dodger.style.right = `${right-1}px`}}
+
+document.addEventListener('keydown',function(e){if(e.which===37){moveDodgerLeft();}if(e.which===39){moveDodgerRight();}})
